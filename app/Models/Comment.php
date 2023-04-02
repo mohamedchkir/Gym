b6c900a71539467965b0871ff6e2ca43d1e38d0e
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-04-02 00:37:16 +0000

    travaillz

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'text',
+        'user_id',
+        'product_id',
+        'programme_id',
+        'user_id',
+    ];
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    
+    public function programme()
+    {
+        return $this->belongsTo(Programme::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'quantity',
+        'price',
+        'image',
+        'description',
+    ];
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
