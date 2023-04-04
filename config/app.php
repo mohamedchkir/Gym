f6e008352e5e10981f4805656cd276e3835502a8
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-04-04 14:30:25 +0000

    ajouter les parmission et assigner chaque permission à un utilisateur

--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Spatie\Permission\PermissionServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
 
     ],
 
