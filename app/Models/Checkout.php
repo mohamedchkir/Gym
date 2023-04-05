257671052c46a119e3f18f27b25f7a94c00e38d7
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-04-05 10:06:28 +0000

    travailler la relations many to many in checkout and product tables

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Checkout extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+    ];
+
+
+    public function products()
+       {
+           return $this->belongsToMany(Product::class);
+       }
 }
+
