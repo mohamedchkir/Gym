837a9f7903f41bb5ac41f78bf97a3953de3709e8
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-04-10 14:07:21 +0000

    travailler sur le front end

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,12 +1,16 @@
+// **************This is for the index page ********//
+
+//Menu toggle
 function toggleNavbar(collapseID) {
     document.getElementById(collapseID).classList.toggle('hidden')
     document.getElementById(collapseID).classList.toggle('block')
   }
 
-
+//aos animation
   AOS.init({
     delay: 200,
     duration: 1200,
     once: false,
   })
 
+// **************This is for the dashboard page ********//
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" href="https://www.flaticon.com/svg/static/icons/svg/2913/2913448.svg" type="image/x-icon"/>
         <title>GYM</title>
-
         <!-- Fonts -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -55,17 +54,17 @@
        <li class="flex items-center">
            @if (Route::has('login'))
            @auth
-        <a href="{{ url('/dashboard') }}" class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold">Dashboard</a>
+        <a href="{{ url('/dashboard') }}" class="bg-transparent hover:bg-red-500 text-red-500 font-semibold hover:text-white p-1 border border-red-500 hover:border-transparent rounded inline-block mt-2  cursor-pointer">Dashboard</a>
        </li>
        <!-- Login -->
        <li class="flex items-center">
            @else
-            <a href="{{ route('login') }}" class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold">Log in</a>
+            <a href="{{ route('login') }}" class="bg-transparent hover:bg-red-500 text-red-500 font-semibold hover:text-white p-1 border border-red-500 hover:border-transparent rounded inline-block mt-2  cursor-pointer">Log in</a>
        </li>
        <!-- Register -->
        <li class="flex items-center">
            @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold">Register</a>
+            <a href="{{ route('register') }}" class="bg-transparent hover:bg-red-500 text-red-500 font-semibold hover:text-white p-1 border border-red-500 hover:border-transparent rounded inline-block mt-2 lg:mx-4 cursor-pointer">Register</a>
             @endif
         </li>
 
@@ -83,7 +82,7 @@
    style="min-height: 95vh"
  >
    <div class="absolute top-0 w-full h-full bg-top bg-cover" style="
-       background-image: url('https://images.unsplash.com/photo-1526506118085-60ce8714f8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80');">
+       background-image: url('https://mattsko.files.wordpress.com/2021/07/0-muscle-3837833.jpg?w=634');">
      <span id="blackOverlay"class="w-full h-full absolute opacity-75 bg-black"></span>
    </div>
    <div class="container relative mx-auto" data-aos="fade-in">
@@ -94,15 +93,10 @@
              Feel The <span class="text-red-500">Power</span>
            </h1>
            <p class="mt-4 text-lg text-gray-300">
-             Welcome to The Power Room. We are a fitness and training
+             Welcome to Master GYM. We are a fitness and training
              center that focuses on pushing you to your absolute limit.
              Download our complete brochure to get started today!
            </p>
-           <a
-             href="#"
-             class="bg-transparent hover:bg-red-500 text-red-500 font-semibold hover:text-white p-4 border border-red-500 hover:border-transparent rounded inline-block mt-5 cursor-pointer"
-             >Join Us</a
-           >
          </div>
        </div>
      </div>
@@ -136,7 +130,7 @@
          <img
            alt="..."
            class="max-w-full rounded-lg shadow-lg"
-           src="https://images.unsplash.com/photo-1550345332-09e3ac987658?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+           src="https://images.unsplash.com/photo-1612099197070-4db4ab9abcd4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"
          />
        </div>
        <div
@@ -144,7 +138,7 @@
          data-aos="fade-left"
        >
          <div class="md:pr-12">
-           <small class="text-red-500 ">About our gym</small>
+           <small class="text-red-500 text-lg">About our gym</small>
            <h3 class="text-4xl uppercase font-bold">Safe Body Building</h3>
            <p class="mt-4 text-lg leading-relaxed">
              The extension comes with three pre-built pages to help you get
@@ -156,7 +150,7 @@
                <div class="flex items-center">
                  <div>
                    <span
-                     class="font-semibold inline-block py-3 mr-3 text-red-500"
+                     class="font-semibold inline-block py-3 mr-3 text-white"
                      ><i class="fas fa-dumbbell fa-2x"></i
                    ></span>
                  </div>
@@ -171,7 +165,7 @@
                <div class="flex items-center">
                  <div>
                    <span
-                     class="font-semibold inline-block py-3 mr-3 text-orange-500"
+                     class="font-semibold inline-block py-3 mr-3 text-white"
                      ><i class="fas fa-hard-hat fa-2x"></i
                    ></span>
                  </div>
@@ -186,12 +180,12 @@
                <div class="flex items-center">
                  <div>
                    <span
-                     class="font-semibold inline-block py-3 mr-3 text-orange-500"
+                     class="font-semibold inline-block py-3 mr-3 text-white"
                      ><i class="fas fa-users fa-2x"></i
                    ></span>
                  </div>
                  <div>
-                   <h4 class="text-xl">3 professional trainers</h4>
+                   <h4 class="text-xl">Professional trainers</h4>
                  </div>
                </div>
              </li>
@@ -201,6 +195,47 @@
      </div>
    </div>
  </section>
+
+ <!-- ChoseUs Section Begin -->
+<section class="choseus-section py-16">
+    <div class="container mx-auto">
+        <div class="mb-12 text-center">
+            <span class="text-gray-600 text-lg font-medium">Why chose us?</span>
+            <h2 class="text-3xl font-bold mt-2">PUSH YOUR LIMITS FORWARD</h2>
+        </div>
+        <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+            <div class="text-center">
+                <span class="block mb-4 text-5xl text-red-500">
+                    <i class="fa-sharp fa-solid fa-medal"></i>
+                </span>
+                <h4 class="text-lg font-medium mb-2">Modern equipment</h4>
+                <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
+            </div>
+            <div class="text-center">
+                <span class="block mb-4 text-5xl text-red-500">
+                    <i class="fa-solid fa-bowl-food"></i>
+                </span>
+                <h4 class="text-lg font-medium mb-2">Healthy nutrition plan</h4>
+                <p class="text-gray-500 text-sm">Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+            </div>
+            <div class="text-center">
+                <span class="block mb-4 text-5xl text-red-500">
+                    <i class="fa-solid fa-dumbbell"></i>
+                </span>
+                <h4 class="text-lg font-medium mb-2">Professional training plan</h4>
+                <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut dolore facilisis.</p>
+            </div>
+            <div class="text-center">
+                <span class="block mb-4 text-5xl text-red-500">
+                    <i class="fa-solid fa-heart-pulse"></i>
+                </span>
+                <h4 class="text-lg font-medium mb-2">Unique to your needs</h4>
+                <p class="text-gray-500 text-sm">Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
 
  <!-- Trainers Section -->
  <section class="pt-20 pb-48">
@@ -246,7 +281,7 @@
          <div class="px-6">
            <img
              alt="..."
-             src="https://images.unsplash.com/photo-1594381898411-846e7d193883?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+             src="https://images.unsplash.com/photo-1606902965551-dce093cda6e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
              class="shadow-lg rounded max-w-full mx-auto"
              style="max-width: 250px"
            />
@@ -324,7 +359,7 @@
            class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300"
            data-aos="fade-up-right"
          >
-           <div class="flex-auto p-5 lg:p-10 bg-orange-500 text-white">
+           <div class="flex-auto p-5 lg:p-10 bg-red-500 text-white">
              <h4 class="text-2xl font-semibold">Want to work with us?</h4>
              <p class="leading-relaxed mt-1 mb-4">
                Complete this form and we will get back to you in 24 hours.
@@ -407,27 +442,24 @@
    <div class="flex flex-wrap">
      <div class="w-full lg:w-6/12 px-4">
        <h4 class="text-3xl font-semibold">Follow us on social media</h4>
-       <h5 class="text-lg mt-0 mb-2 text-gray-700">
-         Find us on any of these platforms, we respond 1-2 business days.
-       </h5>
        <div class="mt-6">
          <button
            class="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
            type="button"
          >
-           <i class="flex fab fa-twitter text-orange-500"></i></button
+           <i class="flex fab fa-twitter text-red-500"></i></button
          ><button
            class="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
            type="button"
          >
            <i
-             class="flex fab fa-facebook-square text-orange-500"
+             class="flex fab fa-facebook-square text-red-500"
            ></i></button
          ><button
            class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 p-3"
            type="button"
          >
-           <i class="flex fab fa-linkedin text-orange-500"></i>
+           <i class="flex fab fa-linkedin text-red-500"></i>
          </button>
        </div>
      </div>
@@ -436,9 +468,9 @@
    <div
      class="flex flex-wrap items-center md:justify-between justify-center"
    >
-     <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+     <div class="w-full px-4 mx-auto text-center">
        <div class="text-sm text-gray-600 font-semibold py-1">
-         Copyright © The Power Room
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by Chkir
        </div>
      </div>
    </div>
@@ -451,15 +483,5 @@
     integrity="sha512-A7AYk1fGKX6S2SsHywmPkrnzTZHrgiVT7GcQkLGDe2ev0aWb8zejytzS8wjo7PGEXKqJOrjQ4oORtnimIRZBtw=="
     crossorigin="anonymous"></script>
         <!-- AOS Script -->
-    <script>
-function toggleNavbar(collapseID) {
-    document.getElementById(collapseID).classList.toggle('hidden')
-    document.getElementById(collapseID).classList.toggle('block')
-  }
-
-        AOS.init({
-            delay: 200,
-            duration: 1200,
-            once: false,})
-    </script>
+<script src="./assets/js/index.js"></script>
 </html>
