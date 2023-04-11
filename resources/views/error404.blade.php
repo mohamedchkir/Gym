3ec461350668812f6598ecaa16c599fd795f6219
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-04-11 10:38:52 +0000

    travailler suR page Error 404

--- resources/views/error404.blade.php
+++ resources/views/error404.blade.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Error 404</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="https://www.flaticon.com/svg/static/icons/svg/2913/2913448.svg" type="image/x-icon"/>
+        <title>Error 404</title>
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+         <!-- Tailwind-->
+        <script src="https://cdn.tailwindcss.com"></script>
+        <!-- Styles -->
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
 <body>
-    <div class="min-h-screen flex items-center justify-center bg-primary">
+    <div class="min-h-screen flex items-center justify-center bg-red-600">
         <div class="max-w-full lg:w-7/12 text-white">
           <div class="flex flex-col lg:flex-row items-center">
             <div class="lg:w-1/2 text-right pr-4">
               <h1 class="text-8xl mb-0">404</h1>
             </div>
-            <div class="lg:w-1/2 pl-4">
+            <div class="lg:w-1/2  pl-4">
               <h2 class="text-4xl">SORRY!</h2>
               <h3 class="text-2xl font-light">The page you’re looking for was not found.</h3>
             </div>
