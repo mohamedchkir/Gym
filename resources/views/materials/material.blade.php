e8487c3cda4cd2f856e86daa0e430cc8c8343695
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-04-12 09:43:01 +0000

    work in  material edit full input with ajax

--- resources/views/materials/material.blade.php
+++ resources/views/materials/material.blade.php
@@ -109,7 +109,7 @@
                 {{$material->created_at->diffForHumans()}}
             </td>
             <td class="px-6 space-x-2 whitespace-nowrap">
-                <button type="button" data-modal-toggle="edit-user-modal" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button type="button" data-modal-toggle="material-modal" onclick="editematerial({{$material->id}})" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
                     Edit material
                 </button>
