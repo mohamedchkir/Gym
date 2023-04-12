3b7182f1dcb7aa32cde8d5f791f6febd76ddee43
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-04-12 14:26:18 +0000

    work in user crud in controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $users = User::all();
+        return view('users', compact('users'));
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $name = '';
+        $file = $request->image;
+        $name = $file->getClientOriginalName();
+        $file->move(public_path('assets/images/users'), $name);
+
+
+        //store data
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'phone' => $request->phone,
+            'image' => 'assets/images/products/' . $name,
+            'created_at' => Carbon::now(),
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $user=User::find($id);
+        // return json response to ajax
+        return response()->json($user);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+        $image = $request->image;
+        if ($request->hasFile('image')) {
+            $file = $request->image;
+            $image = $file->getClientOriginalName();
+            $file->move(public_path('assets/images/users'), $image);
+            $image='assets/images/users/'.$image;
+
+        }
+            $user->name = $request->name;
+            $user->image = $image;
+            $user->email = $request->email;
+            $user->phone = $request->phone;
+            $user->password = $request->password;
+            $user->updated_at = Carbon::now();
+            $user->save();
+            return redirect()->back()->with('warning', 'Update successfully');
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $user = User::find($id);
+        $user->delete();
+
+    }
+}
---
+++ resources/views/components/timetable.blade.php
@@ -0,0 +1,64 @@
+<section class="class-timetable-section spad">
+    <div class="container">
+      <div class="flex flex-col lg:flex-row">
+        <div class="lg:w-6/12 mb-8 lg:mb-0">
+          <div class="section-title">
+            <span>Find Your Time</span>
+            <h2>Find Your Time</h2>
+          </div>
+        </div>
+        <div class="lg:w-6/12">
+          <div class="table-controls">
+            <ul class="flex items-center justify-end space-x-4 text-gray-600">
+              <li class="active cursor-pointer" data-tsfilter="all">All event</li>
+              <li class="cursor-pointer" data-tsfilter="fitness">Fitness tips</li>
+              <li class="cursor-pointer" data-tsfilter="motivation">Motivation</li>
+              <li class="cursor-pointer" data-tsfilter="workout">Workout</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="table-responsive">
+        <table class="w-full">
+          <thead>
+            <tr class="text-left text-gray-600 border-b border-gray-300">
+              <th></th>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+              <th>Thursday</th>
+              <th>Friday</th>
+              <th>Saturday</th>
+              <th>Sunday</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="text-left">
+              <td class="class-time py-4 px-2 border-b border-gray-300">6.00am - 8.00am</td>
+              <td class="dark-bg hover-bg ts-meta py-4 px-2 border-b border-gray-300" data-tsmeta="workout">
+                <h5>WEIGHT LOOSE</h5>
+                <span>RLefew D. Loee</span>
+              </td>
+              <td class="hover-bg ts-meta py-4 px-2 border-b border-gray-300" data-tsmeta="fitness">
+                <h5>Cardio</h5>
+                <span>RLefew D. Loee</span>
+              </td>
+              <td class="dark-bg hover-bg ts-meta py-4 px-2 border-b border-gray-300" data-tsmeta="workout">
+                <h5>Yoga</h5>
+                <span>Keaf Shen</span>
+              </td>
+              <td class="hover-bg ts-meta py-4 px-2 border-b border-gray-300" data-tsmeta="fitness">
+                <h5>Fitness</h5>
+                <span>Kimberly Stone</span>
+              </td>
+              <td class="dark-bg blank-td py-4 px-2 border-b border-gray-300"></td>
+              <td class="hover-bg ts-meta py-4 px-2 border-b border-gray-300" data-tsmeta="motivation">
+                <h5>Boxing</h5>
+                <span>Rachel Adam</span>
+              </td>
+              <td class="dark-bg hover-bg ts-meta py-4 px-2 border-b border-gray-300" data-tsmeta="workout">
+                <h5>Body Building</h5>
+                <span>Robert Cage</span>
+              </td>
+            </tr>
+            <tr class="text-left">
