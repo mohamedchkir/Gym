6e8040e6b7354e9d4913134c411857a3453fb02a
Mohamed Chkir <mohamedchkir2@gmail.com>
2023-04-27 02:14:35 +0100

    travailler l'affichage des prix de produits et total dans  carts dans le paanier

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -76,18 +76,31 @@
       <div class="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
         <div class="mb-2 flex justify-between">
           <p class="text-gray-700">Subtotal</p>
-          <p class="text-gray-700">$129.99</p>
+          @php $total = 0 @endphp
+                        @if(session('cart'))
+                          @foreach(session('cart') as $id => $item)
+
+                              @php $total += $item['price'] * $item['quantity'] @endphp
+                          @endforeach
+                        @endif
+          <p class="text-gray-700">{{$total}} DH</p>
         </div>
         <div class="flex justify-between">
-          <p class="text-gray-700">Shipping</p>
-          <p class="text-gray-700">$4.99</p>
+            @php $Donnation = 1 @endphp
+                        @if(session('cart'))
+                          @foreach(session('cart') as $id => $item)
+
+                              @php $total=$total + $Donnation @endphp
+                          @endforeach
+                        @endif
+          <p class="text-gray-700">Donnation</p>
+          <p class="text-gray-700">{{$Donnation}} DH</p>
         </div>
         <hr class="my-4" />
         <div class="flex justify-between">
           <p class="text-lg font-bold">Total</p>
           <div class="">
-            <p class="mb-1 text-lg font-bold">$134.98 USD</p>
-            <p class="text-sm text-gray-700">including VAT</p>
+            <p class="mb-1 text-lg font-bold">{{$total}} MAD</p>
           </div>
         </div>
         <button class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check out</button>
